53fa559a7d41d4bf2d979a654ccefa66e1c9dd0f
trliamg <52527207+demirkolorg@users.noreply.github.com>
2023-04-05 14:34:16 +0300

    24

--- 24. Gün (Element)/script.js
+++ 24. Gün (Element)/script.js
@@ -1,1 +1,22 @@
 
+const h1=document.querySelector('h1')
+console.log(h1.attributes);
+console.log('id.value:',h1.attributes.id.value);
+console.log('id.style:',h1.attributes.style.value);
+
+for (const attr of h1.attributes) {
+  console.log('name: ',attr.name,':value: ',attr.value);
+}
+
+
+const ul=document.querySelector('ul')
+const deleteBtn=document.querySelectorAll('.delete-btn')
+
+deleteBtn.forEach(btn => {
+  btn.addEventListener('click', e=>{
+    e.target.closest('li').remove()
+    if(ul.childElementCount===0){
+      ul.innerHTML='Gösterilecek veri bulunamadı'
+    }
+  })
+}) 
